3c968ee8cec79dc7c2117938fcd45619a6554824
maheshpatole123 <123371677+maheshpatole123@users.noreply.github.com>
2023-01-24 15:55:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,303 @@
+// import React from "react";
+
+import './App.css'
+// import Parent from './Component/Day23/Memo';
+// import UseEffectHook from './Component/Day14/UseEffectHook';
+
+// import Apilistrendering from './practice/Apilistrendering';
+// //from react-router-dom 
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from 'react-router-dom';
+
+//import Welcome from "./Component/Welcome.js"
+
+//import Greet from "./Component/Greet"
+// import HelloComponenet from "./Component/Day4/HelloComponenet"
+
+// import JsxComponent from "./Component/Day5/jsxComponents";
+
+// import PropsComponent from "./Component/Day5/PropsComponent";
+
+// import PropsClassComponent from "./Component/Day6/PropsClassComponent.jsx";
+
+// import PropsDestructuingClassComp from "./Component/Day6/PropsDestructuingClassComp.jsx";
+
+// import PropsDestructuringFunction from "./Component/Day6/PropsDestructuringFunctionalComp.jsx";
+// import PropsDestructuringFunctionalComp from "./Component/Day6/PropsDestructuringFunctionalComp.jsx";
+
+// import StateReactComp from "./Component/Day6/StateReactComp.jsx";
+
+// import EventBinding from "./Component/Day7/EventBinding.jsx";
+
+// import ReactStatelnClassComp from "./Component/Day7/ReactStatelnClassComp.jsx";
+
+// //import ParentComp from "./Component/Day8/ParentComp.jsx";
+
+// //import ChildComp from "./Component/Day8/ChildComp.jsx";
+
+//import ConditinalRenderingInReact from "./Component/Day8/ConditinalRenderingInReact.jsx";
+
+// import ListRendering from "./Component/Day8/ListRendering";
+
+// import FromBasics from "./Component/Day9/FromBasics";
+
+
+// import CompStyling from "./Component/Day9/CompStyling";
+
+// import ClassCompLifeCycleMethod from "./Component/Day10/classCompLifeCycleMethod";
+
+// import PureComp from "./Component/Day11/PureComp";
+
+// import RefsInClassComp from "./Component/Day11/RefsInClassComp";
+
+
+// import PropsDrillingInReact from "./Component/Day12/PropsDrillingInReact";
+
+
+// import RefsInClassComp from "./Component/Day12/RefsInClassComp";
+
+// import ReactClassComp from "./Component/Day13/ReactClassComp";
+
+// import ReactHooks from "./Component/Day13/ReactHooks";
+
+// import HooksInReact from "./Component/Day14/HooksInReact";
+
+// import UseContextHook from "./Component/Day14/UseContextHook";
+
+
+// import UseEffectHook from "./Component/Day14/UseEffectHook";
+
+// import UseRefHook from "./Day15/UseRefHook";
+
+//Routting
+
+// import Home from "./Day15/Home";
+// import About from "./Day15/About";
+// import Services from "./Day15/Services";
+// import Contactus from "./Day15/Contactus";
+
+// import Employees from "./Component/Day16/Employees";
+// import EmployeeStore from "./Component/Day16/EmployeeStore";
+
+// import EmployeeStore from "./Component/practice/EmployeeStore";
+
+// import TodayPractice from "./Component/TodayPractice";
+
+// ----------------------------------practice-------------------------------------------
+
+// import PureComp from "./practice/PureComp";
+// import ClassComLifeCycleMethod from "./practice/ClassComLifeCycleMethod";
+// import RefsInClassCom from "./practice/RefsInClassCom";
+// import ClassComLifeCycleMethod from "./practice/ClassComLifeCycleMethod";
+// import ListRendering from "./practice/ListRendering";
+
+// import Apilistrendering from './practice/Apilistrendering';
+
+// import PracticeToday from './practice/PracticeToday';
+
+//from react-router-dom
+
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from 'react-router-dom';
+
+// import About from './practice/Day15/About';
+// import Contactus from './practice/Day15/Contactus';
+// import Services from './practice/Day15/Services';
+// import Home from './practice/Day15/Home';
+// import React from 'react';
+
+// import UseRefHook from './practice/Day15/UseRefHook';
+
+// import PropsDrilingInReact from './practice/Day12/PropsDrilingInReact';
+
+// import UseStateInHook from './practice/Day12/UseStateInHook';
+
+// import UseEffectHooks from './practice/Day12/UseEffectHooks';
+
+// import TodayPresentation from './practice/Day12/TodayPresentation';
+
+// import AddPost from './practice/Day19/AddPost';
+
+// import Deletbutton from './practice/Day19/Deletbutton';
+// import AddNewPostButton from './practice/Day19/AddNewPostButton';
+
+// import Main from './practice/Day19/Main';
+
+// import Post from './practice/Crud Class Comp/post-main';
+
+// import Employeestore from './practice/Day22/employee-store';
+
+// import EmployeeMain from './practice/Day22/employee-main';
+// ----------------today practice on 11012023
+
+// import TodayPractice from './practice11012023/TodayPractice';
+
+import TodayPractice from './practice/Practice21012023/TodayPractice';
+
+//----------------------------Day23-----------------------
+
+// import UseReducerHook from './Component/Day23/UseReducerHook';
+
+// import Memo from './Component/Day23/Memo'
+
+function App() {
+  return (
+
+
+    <div className="App" >
+
+      <TodayPractice name='mahesh' lname='patole' />
+      {/* todaypractice11012023 */}
+      {/* <TodayPractice name='mahesh patole' edu='M.sc Mathematics' RgNo='EP-2022/023' fname='Amit' mname='Vishnu' lname='Zambare' /> */}
+
+      {/* <UseReducerHook /> */}
+      {/* <Memo /> */}
+
+
+
+      {/* ----------------------------------prctice ----------------------------------------------*/}
+      {/* <PureComp /> */}
+      {/* <UseRefHook /> */}
+
+      {/* <RefInClass /> */}
+
+
+      {/* <PropsDrilingInReact /> */}
+      {/* <TodayPractice name='Mahesh' lname="Patole " mblNo='9511830988' /> */}
+
+      {/* <RefsInClassCom /> */}
+      {/* <ListRendering /> */}
+      {/* <ClassComLifeCycleMethod /> */}
+
+      {/* <Apilistrendering /> */}
+
+      {/* <PracticeToday /> */}
+
+      {/* -----------------------Router----------------- */}
+      {/* <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+            <li>
+              <Link to="/service">Services</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+
+          </ul>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/service" element={<Services />}></Route>
+            <Route exact path="/contact" element={<Contactus />}></Route>
+          </Routes>
+        </div>
+
+      </Router> */}
+
+
+
+
+      {/* --------------------------practice ----------------------------------------*/}
+
+      {/* <TodayPresentation */}
+
+      {/* <AddPost /> */}
+
+      {/* <AddTable /> */}
+
+      {/* <Main /> */}
+      {/* <Deletbutton /> */}
+
+      {/* <Post /> */}
+      {/* <EmployeeStore /> */}
+      {/* <Employeestore /> */}
+
+      {/* <EmployeeMain /> */}
+
+
+
+      {/* < Welcome />
+      < Greet />
+      <HelloComponenet /> */}
+
+      {/* <UseStateInHook /> */}
+
+      {/* <UseEffectHook /> */}
+
+      {/* <JsxComponent />
+
+      < PropsComponent /> */}
+
+      {/* <PropsClassComponent message={"this is props passed from app comp  "} /> */}
+
+      {/* <PropsDestructuingClassComp name={'Mahesh '} city={'Solapur'} company={'Shriram College of Agricultural Engineering,Paniv'} />
+
+      <PropsDestructuringFunctionalComp name={'Arohi Patole'} city={'Solapur'} company={'Lokvikas English Medium School, Velapur'} /> */}
+
+      {/* <StateReactComp /> */}
+
+      {/* <EventBinding /> */}
+
+      {/* <ReactStatelnClassComp /> */}
+
+      {/* <ChildComp />
+
+      <ParentComp /> */}
+
+      {/* <ConditinalRenderingInReact /> */}
+
+      {/* <ListRendering /> */}
+
+      {/* <CompStyling /> */}
+
+      {/* <FromBasics /> */}
+
+
+      {/* < ClassCompLifeCycleMethod /> */}
+
+      {/* <PureComp /> */}
+      {/* <PropsDrillingInReact /> */}
+
+      {/* <ReactClassComp /> */}
+
+      {/* <RefsInClassComp /> */}
+
+      {/* <ReactHooks /> */}
+
+      {/* <HooksInReact /> */}
+
+      {/* <UseContextHook /> */}
+
+      {/* <UseEffectHook /> */}
+
+      {/* <UseRefHook /> */}
+
+      {/* <Home />
+      <About />
+      <Services />
+      < Contactus /> */}
+
+      {/* <Employees /> */}
+
+      {/* <EmployeeStore /> */}
+    </div >
+
+  );
+}
+
+export default App;
